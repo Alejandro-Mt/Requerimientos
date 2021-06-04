18db3095c0526169fe0968ff7037a51b29468c6c
Alejandro Martinez Garcia <alejandro.garcia@triplei.mx>
2021-06-04 11:55:49 -0500

    message

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,4 @@
+<footer class="footer text-center">
+    Expertos en la Administración y Proyección de Personal y Marcas Comerciales<a
+        href="https://www.triplei.mx/">Triple I</a>.
+</footer>
