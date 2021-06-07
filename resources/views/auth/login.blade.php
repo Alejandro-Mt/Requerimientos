29b490ba65276f3bc8f6cb172f44242d3981c455
Alejandro Martinez Garcia <alejandro.garcia@triplei.mx>
2021-06-07 17:45:46 -0500

    message

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,14 +1,5 @@
 @extends('layouts.app')
 
-<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
-<link rel="stylesheet" type="text/css" href="{{asset("vendor/bootstrap/css/bootstrap.min.css")}}">
-<link rel="stylesheet" type="text/css" href="{{asset("fonts/font-awesome-4.7.0/css/font-awesome.min.css")}}">
-<link rel="stylesheet" type="text/css" href="{{asset("vendor/animate/animate.css")}}">
-<link rel="stylesheet" type="text/css" href="{{asset("vendor/css-hamburgers/hamburgers.min.css")}}">
-<link rel="stylesheet" type="text/css" href="{{asset("vendor/select2/select2.min.css")}}">
-<link rel="stylesheet" type="text/css" href="{{asset("css/util.css")}}">
-<link rel="stylesheet" type="text/css" href="{{asset("css/main.css")}}">
-
 @section('content')
 
 <div class="container-right">
@@ -82,16 +73,6 @@
         </div>
     </div>
 </div>
-<script src="{{asset("vendor/jquery/jquery-3.2.1.min.js")}}"></script>
-<script src="{{asset("vendor/bootstrap/js/popper.js")}}"></script>
-<script src="{{asset("vendor/bootstrap/js/bootstrap.min.js")}}"></script>
-<script src="{{asset("vendor/select2/select2.min.js")}}"></script>
-<script src="{{asset("vendor/tilt/tilt.jquery.min.js")}}"></script>
-<script >
-    $('.js-tilt').tilt({
-        scale: 1.1
-    })
-</script>
-<script src="{{asset("js/main.js")}}"></script>
+
 @endsection
 
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,12 +12,34 @@
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
 
+    <script src="{{asset("vendor/jquery/jquery-3.2.1.min.js")}}"></script>
+    <script src="{{asset("vendor/bootstrap/js/popper.js")}}"></script>
+    <script src="{{asset("vendor/bootstrap/js/bootstrap.min.js")}}"></script>
+    <script src="{{asset("vendor/select2/select2.min.js")}}"></script>
+    <script src="{{asset("vendor/tilt/tilt.jquery.min.js")}}"></script>
+    <script >
+        $('.js-tilt').tilt({
+            scale: 1.1
+        })
+    </script>
+    <script src="{{asset("js/main.js")}}"></script>
+
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    <link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+    <link rel="stylesheet" type="text/css" href="{{asset("vendor/bootstrap/css/bootstrap.min.css")}}">
+    <link rel="stylesheet" type="text/css" href="{{asset("fonts/font-awesome-4.7.0/css/font-awesome.min.css")}}">
+    <link rel="stylesheet" type="text/css" href="{{asset("vendor/animate/animate.css")}}">
+    <link rel="stylesheet" type="text/css" href="{{asset("vendor/css-hamburgers/hamburgers.min.css")}}">
+    <link rel="stylesheet" type="text/css" href="{{asset("vendor/select2/select2.min.css")}}">
+    <link rel="stylesheet" type="text/css" href="{{asset("css/util.css")}}">
+    <link rel="stylesheet" type="text/css" href="{{asset("css/main.css")}}">
+    
 </head>
 <body>
     <div id="app">
