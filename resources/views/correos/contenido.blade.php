b17b5aba5924f8a2a0ee802a793bf390bd96e9d6
Alejandro-Mt <alejandro.martinez@3ti.mx>
2021-11-18 14:23:13 -0600

    correo

--- resources/views/correos/contenido.blade.php
+++ resources/views/correos/contenido.blade.php
@@ -1,7 +1,7 @@
 @component('mail::message')
 <!-- Encabezado de correo -->
 @foreach($formato as $dato)
-<h2 align="center">{{$dato->folio}} {{$dato->descripcion}}</h2>
+<h2 align="center" width="300">{{$dato->folio}} {{$dato->descripcion}}</h2>
 <table>
 <tr>
 <img style="margin: 0px 10px 1Opx 0px;" src="{{asset("assets/images/new_logo_3ti.png")}}" alt="logo" width="150" height="60"/>
