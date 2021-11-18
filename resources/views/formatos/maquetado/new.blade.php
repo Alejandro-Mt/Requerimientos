72555e42cfb43ed8d59373bf513ad3358b1ae4a6
Alejandro-Mt <alejandro.martinez@3ti.mx>
2021-11-18 10:48:39 -0600

    error en Cliente

--- resources/views/formatos/maquetado/new.blade.php
+++ resources/views/formatos/maquetado/new.blade.php
@@ -7,7 +7,7 @@
             <p>(*) Campos Obligatorios</p>
             <h6 class="card-subtitle"></h6>
             <form method="POST" action="{{route ('NRegistro')}}" class="mt-5">
-                {{ csrf_token() }}
+                {{ csrf_field() }}
                 <div>
                     <section>
                         <div class="d-none">
