d383bd41230d48ca37ea1127bfd02bd3efbfe4c4
Alejandro-Mt <alejandro.martinez@3ti.mx>
2021-11-23 16:21:08 -0600

    login

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -35,8 +35,8 @@
           <!-- Toggle which is visible on mobile only -->
           <!-- ============================================================== -->
           <a class="topbartoggler d-block d-md-none waves-effect waves-light" href="javascript:void(0)" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-              <!--<i class="ri-more-line fs-6">-->
-              <i class="ti-menu ti-close"></i>
+              <i class="ri-more-line fs-6">
+              <!--<i class="ti-menu ti-close"></i>-->
           </a>
       </div>
       <!-- ============================================================== -->
