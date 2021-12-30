4b42e631a78ee187d2b30fabc8ef542c43d600d7
Alejandro-Mt <alejandro.martinez@3ti.mx>
2021-12-30 07:33:24 -0600

    ajustes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -43,7 +43,7 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-dark bg-dark shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/home') }}">
+                <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Indicadores') }} <!-- ERROR EN ETIQUETA-->
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
