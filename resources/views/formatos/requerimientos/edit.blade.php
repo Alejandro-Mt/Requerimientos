19d4d37456fdab2f6280077652a929d2f8b04bed
Alejandro-Mt <84807931+Alejandro-Mt@users.noreply.github.com>
2022-02-13 12:55:38 -0600

    filtros

--- resources/views/formatos/requerimientos/edit.blade.php
+++ resources/views/formatos/requerimientos/edit.blade.php
@@ -36,6 +36,51 @@
             <div class="button-group">
                 <button id="pestana1" type="button" class="btn btn-light-primary text-primary px-4 rounded-pill font-medium collapsed">Análisis</button>
                 <button id="pestana2" type="button" class="btn btn-light-success text-success px-4 rounded-pill font-medium collapsed">Requerimientos</button>
+                <button type="button" class="btn text-gray dropdown-toggle" data-bs-toggle="collapse" data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-filter feather-sm"><polygon points="22 3 2 3 10 12.46 10 19 14 21 14 12.46 22 3"></polygon></svg>
+                </button>
+                <div class="collapse" id="collapseExample">
+                    <div class="button-group">
+                        <div class="btn-group mb-2" data-mdb-perfect-scrollbar="true" data-mdb-suppress-scroll-x="true">
+                            <button type="button" class="btn text-gray dropdown-toggle" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                Estatus
+                            </button>
+                            <ul class="dropdown-menu scrollable-menu">
+                                @foreach ($estatus as $e)
+                                <div class="table-responsive">
+                                    <li class="list-inline-item">
+                                        <a class="dropdown-item" href="#"> 
+                                            <div class="form-check mr-sm-2">
+                                            <input id="estatus{{$loop->iteration}}" type="checkbox" class="form-check-input ches">
+                                            <label id="les{{$loop->iteration}}" class="form-check-label" for="estatus">{{$e->titulo}}</label>
+                                            </div>
+                                        </a>
+                                    </li>
+                                </div>
+                                @endforeach
+                            </ul>
+                        </div>
+                        <div class="btn-group mb-2">
+                            <button type="button" class="btn text-gray dropdown-toggle" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                Sistema
+                            </button>
+                            <ul class="dropdown-menu scrollable-menu">
+                                @foreach ($sistemas as $sistema)
+                                <div class="table-responsive">
+                                    <li class="list-inline-item">
+                                        <a class="dropdown-item" href="#"> 
+                                            <div class="form-check">
+                                            <input id="sistema{{$loop->iteration}}" type="checkbox" class="form-check-input chsi">
+                                            <label id="lsi{{$loop->iteration}}" class="form-check-label" for="invalidcheck3">{{$sistema->nombre_s}}</label>
+                                            </div>
+                                        </a>
+                                    </li>
+                                </div>
+                                @endforeach
+                            </ul>
+                        </div>
+                    </div>
+                </div>
             </div>
             <!--<div id='pestana' class="navbar navbar-expand-lg navbar-dark bg-success">
                 <div id='lista' class="navbar-item">
@@ -62,12 +107,14 @@
                         <tr>
                             <th scope="col">Folio</th>
                             <th scope="col">Estatus</th>
+                            <th scope="col">Sistema</th>
                             <th scope="col">Acción</th>
                         </tr>
                     </thead>
-                    <tbody id="contenidopestanas">
+                    <tbody id="searchable">
                         @foreach ($registros as $registro)
                             <tr id="{{$registro->folio}}"class="collapse show" onmousemove="lock('play{{$loop->iteration}}','btn{{$loop->iteration}}')">
+                                <!-- Folio -->
                                 <td>
                                     <div class="form-group row">
                                         <div class="col-md-13" >
@@ -83,7 +130,17 @@
                                         </div>
                                     </div>
                                 </td>
+                                <!-- Titulo -->
                                 <td class="">{{$registro->titulo}}</td>
+                                <!-- Sistema -->
+                                <td>
+                                    @foreach ($sistemas as $sistema) 
+                                        @if ($registro->id_sistema == $sistema->id_sistema)
+                                            {{$sistema->nombre_s}}
+                                        @endif
+                                    @endforeach
+                                </td>
+                                <!-- Accion -->
                                 @switch($registro->id_estatus)
                                     @case(17)
                                         <td><button id="btn{{$loop->iteration}}" type="submit" class="btn btn-warning text-white" ><a href="{{route('Formato',$registro->id_registro)}}" style="color:white">Llenar Solicitud</a></button></td>
@@ -131,30 +188,31 @@
                                     @break
                                     @default 
                                 @endswitch
-                            <td>
-                                @if ($registro->id_estatus <> 18)
-                                    <div class="form-group row">
-                                        <div class="col-md-2 col-lg-1 f-icon">
-                                            <a class="fas fa-plus" href="{{route('Subproceso',$registro->folio)}}" role="button" style="color:#3e5569"></a> 
+                                <!-- menu -->
+                                <td>
+                                    @if ($registro->id_estatus <> 18)
+                                        <div class="form-group row">
+                                            <div class="col-md-2 col-lg-1 f-icon">
+                                                <a class="fas fa-plus" href="{{route('Subproceso',$registro->folio)}}" role="button" style="color:#3e5569"></a> 
+                                            </div>
+                                            @foreach ($pausa as $p)
+                                                @if ($p->pausa == '1' and $p->folio==$registro->folio)
+                                                    <div class="col-md-2 col-lg-2 f-icon">
+                                                        <a id="play{{$loop->iteration}}" class="fas fa-play" style="color:green" href="{{route('Play',$registro->folio)}}"></a>
+                                                    </div>
+                                                @elseif ($p->pausa <> '1' and $p->folio==$registro->folio)
+                                                    <div class="col-md-2 col-lg-2 f-icon">
+                                                        <a id="play{{$loop->iteration}}"class="fas fa-pause"  style="color:red" href="{{route('Pausa',$registro->folio)}}"></a>
+                                                    </div>
+                                                
+                                            @endif
+                                            @endforeach
+                                            <!--<button type="submit" class="btn btn-success text-white">
+                                                <a href="{{route('Subproceso',$registro->folio)}}" style="color:white">Nuevo Subproceso</a>
+                                            </button>-->
                                         </div>
-                                        @foreach ($pausa as $p)
-                                            @if ($p->pausa == '1' and $p->folio==$registro->folio)
-                                                <div class="col-md-2 col-lg-2 f-icon">
-                                                    <a id="play{{$loop->iteration}}" class="fas fa-play" style="color:green" href="{{route('Play',$registro->folio)}}"></a>
-                                                </div>
-                                            @elseif ($p->pausa <> '1' and $p->folio==$registro->folio)
-                                                <div class="col-md-2 col-lg-2 f-icon">
-                                                    <a id="play{{$loop->iteration}}"class="fas fa-pause"  style="color:red" href="{{route('Pausa',$registro->folio)}}"></a>
-                                                </div>
-                                            
-                                        @endif
-                                        @endforeach
-                                        <!--<button type="submit" class="btn btn-success text-white">
-                                            <a href="{{route('Subproceso',$registro->folio)}}" style="color:white">Nuevo Subproceso</a>
-                                        </button>-->
-                                    </div>
-                                @endif
-                            </td>
+                                    @endif
+                                </td>
                             </tr>
                             <tr id="{{$registro->folio}}" class="collapse show"><td></td>
                                 <td id="collapseOne_{{$loop->iteration}}" class="panel-collapse collapse">
@@ -210,8 +268,42 @@
             $('[id^="AA"]').collapse('hide')
             $('[id^="PIP"]').collapse('show')
         });
+        v = $(".ches");
+        for (let i = 0; i <= v.length; i++) {
+            $('#estatus'+i).on('change', function(){
+                if(this.checked){
+                    $('#searchable tr').hide();
+                        $('#searchable tr').filter(function() {
+                            return $(this).find('td').eq(1).text() == document.getElementById('les'+i).innerHTML
+                        }).show();
+                }else{
+                    $('#searchable tr').show();
+                } 
+            });
+        }
+        v = $(".chsi");
+        for (let i = 0; i <= v.length; i++) {
+            $('#sistema'+i).on('change', function(){
+                if(this.checked){
+                    $('#searchable tr').hide();
+                        $('#searchable tr').filter(function() {
+                            console.log($(this).find('td').eq(2).text().replace(/\s+/g, ''));
+                            return $(this).find('td').eq(2).text().replace(/\s+/g, '') == document.getElementById('lsi'+i).innerHTML.replace(/\s+/g, '');
+                        }).show();
+                }else{
+                    $('#searchable tr').show();
+                } 
+            });
+        }
     });
 </script>
+<style>
+    .scrollable-menu {
+    height: auto;
+    max-height: 200px;
+    overflow-x: hidden;
+    }
+</style>
 @endsection
 
 <script type="text/javascript">
@@ -244,5 +336,7 @@
         }
     }
 </script>
+
+
 
 
