8bbc48c85efc9f402eed7cbd0eb9c6f8a9556a68
Alejandro-Mt <84807931+Alejandro-Mt@users.noreply.github.com>
2022-02-28 16:10:28 -0600

    Jerarquia

--- database/migrations/2021_10_20_202401_create_puestos_table.php
+++ database/migrations/2021_10_20_202401_create_puestos_table.php
@@ -16,6 +16,7 @@ class CreatePuestosTable extends Migration
         Schema::create('puestos', function (Blueprint $table) {
             $table->id('id_puesto');
             $table->string('puesto',50);
+            $table->Integer('jerarquia')->length(10)->unsigned()->nullable();
             $table->timestamps();
         });
     }
