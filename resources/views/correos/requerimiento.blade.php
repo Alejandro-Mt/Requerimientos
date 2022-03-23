4a09adf2f59f27c12868b9e9d895cc712b0b878b
Alejandro-Mt <84807931+Alejandro-Mt@users.noreply.github.com>
2022-03-23 09:02:58 -0600

    Actualizacion 1.03.22.21

--- resources/views/correos/requerimiento.blade.php
+++ resources/views/correos/requerimiento.blade.php
@@ -1,11 +1,14 @@
 @component('mail::message')
 @foreach ($datos as $dato)
 # {{$dato->folio}} {{$dato->descripcion}}
+@if ($dato->fechaaut == NULL)
+El cliente ha rechazado la propuesta de requerimiento, se recomienda contactar para mayor información.   
+@else
+El requerimiento ha sido autorizado
 
-El cliente ha rechazado la propuesta de requerimiento, se recomienda contactar para mayor información.
-    
+{{$dato->nombre_r}} {{$dato->apellidos}}
+@endif
 @endforeach
-
 
 <br>
 {{ config('app.name') }}
