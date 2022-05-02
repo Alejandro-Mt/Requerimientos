ce0965cff1007f2c2f3934b21ba783b1684b62c4
Alejandro-Mt <84807931+Alejandro-Mt@users.noreply.github.com>
2022-05-02 12:19:35 -0500

    Actualizacion 1.04.22.25

--- resources/views/formatos/requerimientos/preregistro/carga.blade.php
+++ resources/views/formatos/requerimientos/preregistro/carga.blade.php
@@ -1,27 +1,40 @@
 @extends('layouts.app')
 @section('content')
-  <div class="page-wrapper">
-      <!-- -------------------------------------------------------------- -->
-      <!-- Container fluid  -->
-      <!-- -------------------------------------------------------------- -->
-    <div class="container-fluid">
-      <div class="row">
-        <div class="col-12">
+<link rel="stylesheet" type="text/css" href="{{asset("vendor/bootstrap/css/bootstrap.min.css")}}">
+    
+    
+  <!-- -------------------------------------------------------------- -->
+  <!-- Container fluid  -->
+  <!-- -------------------------------------------------------------- -->
+  <div class="container-fluid">
+    <div class="row">
+        <div class="col-lg-6">
+            <div class="card-body little-profile text-center">
+                <div class="my-3" data-tilt>
+                    <img src="images/img-01.png" alt="IMG">
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-4">
           <div class="card">
             <div class="border-bottom title-part-padding">
               <h4 class="card-title mb-0">{{$folio}}</h4>
             </div>
             <div class="card-body">
-              <h6 class="card-subtitle mb-3">Aqui puedes cargar tus archivos de muestra</h6>
+              <h6 class="card-subtitle mb-3">Cargar archivos</h6>
               <form  class="dropzone" action="{{route('Previsto',$folio)}}" method="post" enctype="multipart/form-data" id="myAwesomeDropzone">
                 {{ csrf_field() }}
               </form>
+              <button type="button" class="login100-form-btn">
+                <a href="{{url('/') }}" style="color:white"> Finalizar</a>
+              </button>
             </div>
           </div>
         </div>
-      </div>
     </div>
   </div>
+  <script src="{{asset("vendor/tilt/tilt.jquery.min.js")}}"></script>
+  <script src="{{asset("vendor/bootstrap/js/bootstrap.min.js")}}"></script>
 
   <link rel="stylesheet" type="text/css" href="{{asset("assets/libs/dropzone/dist/min/dropzone.min.css")}}"/>
   <script src="{{asset("assets/libs/dropzone/dist/min/dropzone.min.js")}}"></script>
