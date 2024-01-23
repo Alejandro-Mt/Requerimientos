42fbb6794736a687599067821ec3ed793ca99c59
Alejandro-Mt <alejandro.martinez@3ti.mx>
2024-01-23 11:20:26 -0600

    correccion de bug en envio de mail

--- app/Mail/Interno/notificacion_definicion.php
+++ app/Mail/Interno/notificacion_definicion.php
@@ -4,6 +4,7 @@ namespace App\Mail\interno;
 
 use App\Models\archivo;
 use App\Models\registro;
+use App\Models\responsable;
 use App\Models\solicitud;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
@@ -25,8 +26,7 @@ class notificacion_definicion extends Mailable
     public function __construct($folio)
     {
         //
-        $this->datos = registro::where('folio',$folio)->first();
-        $this->destinatario = solicitud::where('folior',$folio)->first();
+        $this->datos = registro::join('responsables as r', 'registros.id_responsable', 'r.id_responsable')->where('folio',$folio)->first();
         $this->file = archivo::where('folio',$folio)->
             where('url', 'LIKE', '%Definición de requerimiento%')->
             where('url', 'NOT LIKE', '%versión%')->
--- resources/views/correos/interno/definicion.blade.php
+++ resources/views/correos/interno/definicion.blade.php
@@ -1,8 +1,8 @@
 @component('mail::message')
 
-Estimado {{$destinatario->solicitante}}.,
+Estimado {{$datos->nombre_r}}.,
 
-Espero que estén teniendo un buen día. Les informamos que hemos subido con éxito el archivo 'Definición de requerimiento' a la plataforma y lo hemos enviado al cliente para su revisión y autorización. Este archivo es crucial para nuestro proceso, y agradeceríamos que lo verifiquen.
+Espero que estés teniendo un buen día. Se informa que hemos subido con éxito el archivo 'Definición de requerimiento' a la plataforma y lo hemos enviado al cliente para su revisión y autorización. Este archivo es crucial para nuestro proceso, y agradeceríamos que lo verifiquen.
 
 Gracias por su atención y cooperación en este asunto.
 
